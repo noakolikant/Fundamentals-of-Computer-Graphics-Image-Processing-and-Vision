9ade950c6d079affbf79a5f85ccb969564f2c858
noakolikant <noa.kolikant@gmail.com>
2016-04-09 15:17:11 +0300

    Add Color class and two simple constructors

---
+++ src/RayTracing/Color.java
@@ -0,0 +1,21 @@
+package RayTracing;
+
+public class Color {
+	public byte red;
+	public byte green;
+	public byte blue;
+	
+	public Color()
+	{
+		this.red = 0;
+		this.green = 0;
+		this.blue = 0;
+	}
+	
+	public Color(byte red, byte blue, byte green)
+	{
+		this.red = red;
+		this.blue = blue;
+		this.green = green;
+	}
+}
