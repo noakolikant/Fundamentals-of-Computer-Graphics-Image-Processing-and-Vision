597e4b24a5729a17f4261203f1dc39ec53c6f23d
ophirarie <ophirarie@gmail.com>
2016-04-11 18:59:08 +0300

    find whether a light and a ray has intersection

--- src/RayTracing/LightSource.java
+++ src/RayTracing/LightSource.java
@@ -16,5 +16,20 @@ public class LightSource {
 		this.shadow_intensity = shadow_intensity;
 		this.light_radius = light_radius;
 	}
+	
+	public Boolean has_intersection_point_with_ray(Ray r) {
+		// I've done it pretty ugly in order to avoid sqrt / divide operations.
+		// There must be a prettier way to do it...
+		Vector normalize_position = new Vector(this.position); 
+		normalize_position.substract(r.start); // move start to the origin
+		double dot = normalize_position.dot(r.direction);
+		if (dot < 0) {
+			return false;
+		}
+		double x = r.direction.x_cor * normalize_position.y_cor * normalize_position.z_cor;
+		double y = r.direction.y_cor * normalize_position.x_cor * normalize_position.z_cor;
+		double z = r.direction.z_cor * normalize_position.x_cor * normalize_position.y_cor;
+		return (x==y && x ==z);
+	}
 
 }
