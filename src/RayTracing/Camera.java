aebfe53af8c98eafe86c37e8492a7ea8e2bf4754
noakolikant <noa.kolikant@gmail.com>
2016-04-22 11:20:24 +0300

    Camera's direction is look at point substruct position and not the other way around

--- src/RayTracing/Camera.java
+++ src/RayTracing/Camera.java
@@ -13,8 +13,8 @@ public class Camera {
 		this.position = new Vector(position);
 		this.look_at_point = new Vector(look_at_point);
 		// find the direction vector
-		this.direction = new Vector(position); // copy the vector
-		this.direction.substract(this.look_at_point);
+		this.direction = new Vector(this.look_at_point); // copy the vector
+		this.direction.substract(position);
 		// fix the up vector to be perpendicular to the direction
 		this.up_vector = new Vector(up_vector);
 		Vector temp = new Vector(this.direction); // copy this vector
