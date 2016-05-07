11e2ec828b78464c26e82a999c58a22f3bbed31a
ophirarie <ophirarie@gmail.com>
2016-05-07 14:07:05 +0300

    add plot of points on images

--- src/Ex2/DisplayCorr.m
+++ src/Ex2/DisplayCorr.m
@@ -2,13 +2,31 @@ function [ displayedCorr ] = DisplayCorr(image1, image2, matches, ...
 dist_vals, x)
 %DisplayCorr This function displays a chosen number of correspondences
 
-[dist_vals_sorted, SortIndex] = sort(dist_vals, 'descend');
-matches_sorted = matches(SortIndex, :);
-for i=1:x
-    fprintf('%3.2f,%3.2f -> %3.2f,%3.2f\n', matches_sorted(i,1), ...
-        matches_sorted(i,2), matches_sorted(i,3), matches_sorted(i, 4));    
-end
+    [dist_vals_sorted, SortIndex] = sort(dist_vals, 'descend');
+    matches_sorted = matches(SortIndex, :);
+    for i=1:x
+        fprintf('%3.2f,%3.2f -> %3.2f,%3.2f\n', matches_sorted(i,1), ...
+            matches_sorted(i,2), matches_sorted(i,3), matches_sorted(i, 4));    
+    end
 
+    % Load images
+    im1 = imread(image1);
+    im2 = imread(image2);
+    % Create a new image showing the two images side by side.
+    im3 = appendimages(im1,im2);
+
+    % Show a figure with lines joining the accepted matches.
+    figure('Position', [100 100 size(im3,2) size(im3,1)]);
+    colormap('gray');
+    imagesc(im3);
+    hold on;
+    cols1 = size(im1,2);
+    for i = 1: x
+        plot(matches_sorted(i,2), matches_sorted(i,1),'ro')
+        plot(matches_sorted(i, 4)+cols1, matches_sorted(i,3),'ro')
+        % line([loc1(i,2) loc2(matches(i),2)+cols1], [loc1(i,1) loc2(matches(i),1)], 'Color', 'c');
+    end
+    hold off;
 
 
 
