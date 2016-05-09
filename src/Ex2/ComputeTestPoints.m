3a1a632f5886348946437bc1d771e85fdd5dfeb4
ophirarie <ophirarie@gmail.com>
2016-05-09 20:40:10 +0300

    change compute test points to pick random points

--- src/Ex2/ComputeTestPoints.m
+++ src/Ex2/ComputeTestPoints.m
@@ -1,18 +1,15 @@
 function [ pnts_gt, pnts_computed ] = ComputeTestPoints(H_gt,H_computed)
 %ComputeTestPoints Computes the ground truth points and the points 
 % generated from the H_computed.
-    pnts_gt = zeros(512, 512, 3);
-    pnts_computed = zeros(512,512, 3);
+    NUMBER_OF_RANDOM_POINTS = 100;
+    pnts_gt = zeros(NUMBER_OF_RANDOM_POINTS, 3);
+    pnts_computed = zeros(NUMBER_OF_RANDOM_POINTS, 3);
+    random_points = randi([1, 512], NUMBER_OF_RANDOM_POINTS, 2);
     for i=1:size(pnts_gt, 1)
-        for j=1:size(pnts_gt, 2)
-            pnts_gt(i, j, :) = H_gt * [i, j, 1]';
-            pnts_gt(i, j, 1:3) = pnts_gt(i, j, 1:3) / pnts_gt(i, j, 3);
-            pnts_computed(i, j, :) = H_computed * [i, j, 1]';
-            pnts_computed(i, j, 1:3) = pnts_computed(i, j, 1:3) / ...
-                pnts_computed(i, j, 3);
-        end
+        pnts_gt(i, :) = H_gt * [random_points(i,:), 1]';
+        pnts_gt(i, :) = pnts_gt(i, :) / pnts_gt(i, 3);
+        pnts_computed(i, :) = H_computed * [random_points(i,:), 1]';
+        pnts_computed(i, :) = pnts_computed(i, :) / pnts_computed(i, 3);       
     end
-
-
 end
 
