9ca6b9fb268f77ef350fea743f2df2575f1b81bc
ophirarie <ophirarie@gmail.com>
2016-05-18 23:20:28 +0300

    add files

--- src/Ex2/RANSAC_Wrapper.m
+++ src/Ex2/RANSAC_Wrapper.m
@@ -6,14 +6,7 @@ function [ H_ransac ] = RANSAC_Wrapper(matches, fittingfn, distfn, ...
     
     [x1, T1] = Normalize2dHomPoints(x1);
     [x2, T2] = Normalize2dHomPoints(x2);
-%     fittingfn = @DLT;
-%     distfn = @homdistfn;
-%     degenfn = isdegenerate;
-%     t = 0.6;
-%     s = 4; 
-%     feedback = 0;
-%     maxDataTrials = 100;
-%     maxTrials = 1000;
+
     [H, inliers] = ransac([x1; x2], fittingfn, distfn, degenfn, s, t, feedback, maxDataTrials, maxTrials);
     H_fit = fittingfn([x1(:,inliers)', x2(:,inliers)']);
     
--- src/Ex2/RANSAC_Wrapper_run_file.m
+++ src/Ex2/RANSAC_Wrapper_run_file.m
@@ -9,6 +9,8 @@ maxDataTrials = 100;
 maxTrials = 10000;
 
 H_ransac = RANSAC_Wrapper(matches, fittingfn, distfn, degenfn, s, t, feedback, maxDataTrials, maxTrials);
-new_im_ransac = ComputeProjective(Im, H_ransac); imshow(new_im_ransac)
 [ pnts_gt, pnts_computed ] = ComputeTestPoints(H_gt, H_ransac);
-ComputeError(pnts_gt, pnts_computed) / 100
+error = ComputeError(pnts_gt, pnts_computed) / 100
+
+    
+new_im_ransac = ComputeProjective(Im, H_ransac); imshow(new_im_ransac)
