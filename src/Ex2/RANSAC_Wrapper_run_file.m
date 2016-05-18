9e7638e91c00867f8f66f7160560154db17069e5
ophirarie <ophirarie@gmail.com>
2016-05-18 22:44:58 +0300

    change wrapper

--- src/Ex2/RANSAC_Wrapper_run_file.m
+++ src/Ex2/RANSAC_Wrapper_run_file.m
@@ -2,12 +2,13 @@
 fittingfn = @homfitfn;
 distfn = @homdistfn;
 degenfn = @isdegenerate;
-%degenfn = @(x) 0;
-t = 3;
+t = 0.001;
 s = 4; 
 feedback = 0;
 maxDataTrials = 100;
-maxTrials = 1000;
+maxTrials = 10000;
 
-H_ransac = RANSAC_Wrapper(matches, fittingfn, distfn, degenfn, s, t, feedback, maxDataTrials, maxTrials)
+H_ransac = RANSAC_Wrapper(matches, fittingfn, distfn, degenfn, s, t, feedback, maxDataTrials, maxTrials);
 new_im_ransac = ComputeProjective(Im, H_ransac); imshow(new_im_ransac)
+[ pnts_gt, pnts_computed ] = ComputeTestPoints(H_gt, H_ransac);
+ComputeError(pnts_gt, pnts_computed) / 100
