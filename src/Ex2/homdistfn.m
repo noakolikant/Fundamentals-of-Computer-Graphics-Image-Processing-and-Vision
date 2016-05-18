eb2a5edec838eec0f6dd14541fdbb056b97c169e
ophirarie <ophirarie@gmail.com>
2016-05-18 22:45:31 +0300

    change some params names

--- src/Ex2/homdistfn.m
+++ src/Ex2/homdistfn.m
@@ -3,16 +3,16 @@ function [ inliers, M ] = homdistfn(M, x, t)
     x1 = x(1:3, :);
     x2 = x(4:6, :);
     Mx1 = M*x1;
-    Mx2 = M\x2;
+    invMx2 = M\x2;
 
     for i=1:size(x, 2)
         x1(:, i) = x1(:, i) ./ x1(3, i);
         x2(:, i) = x2(:, i) ./ x2(3, i);
         Mx1(:, i) = Mx1(:, i) ./ Mx1(3, i);
-        Mx2(:, i) = Mx2(:, i) ./ Mx2(3, i);
+        invMx2(:, i) = invMx2(:, i) ./ invMx2(3, i);
      end
 
-    d2 = sum((x1-Mx2).^2)  + sum((x2-Mx1).^2);
+    d2 = sum((x1-invMx2).^2)  + sum((x2-Mx1).^2);
     inliers = find(abs(d2) < t);
 
 end
