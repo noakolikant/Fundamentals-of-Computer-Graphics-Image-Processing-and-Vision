6b075db2b7f9d26552160f865aeb8c075e6f0647
noakolikant <noa.kolikant@gmail.com>
2016-05-21 16:21:13 +0300

    Calculate each pixel's energy

--- src/seamCarving/Pixel.java
+++ src/seamCarving/Pixel.java
@@ -1,5 +1,9 @@
 package seamCarving;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 public class Pixel {
 	int x;
 	int y;
@@ -14,5 +18,39 @@ public class Pixel {
 	{
 		this.x = p.x;
 		this.y = p.y;
+	}
+	
+	public List<Pixel> get_neighbors(int w, int h)
+	{
+		// TODO: I should insert all 8 possible neigbhors and then remove
+		List<Pixel> neighbors_list = new ArrayList<Pixel>();
+		Pixel p1 = new Pixel(this.x - 1, this.y - 1);
+		Pixel p2 = new Pixel(this.x - 1, this.y);
+		Pixel p3 = new Pixel(this.x - 1, this.y);
+		Pixel p4 = new Pixel(this.x, this.y -1);
+		Pixel p5 = new Pixel(this.x, this.y + 1);
+		Pixel p6 = new Pixel(this.x + 1, this.y - 1);
+		Pixel p7 = new Pixel(this.x + 1, this.y);
+		Pixel p8 = new Pixel(this.x + 1, this.y);
+		
+		neighbors_list.add(p1);
+		neighbors_list.add(p2);
+		neighbors_list.add(p3);
+		neighbors_list.add(p4);
+		neighbors_list.add(p5);
+		neighbors_list.add(p6);
+		neighbors_list.add(p7);
+		neighbors_list.add(p8);
+		
+		Iterator<Pixel> it = neighbors_list.iterator();
+		while(it.hasNext())
+		{
+			Pixel p = it.next();
+			if((p.x < 0) || (p.y < 0) || (x > h - 1) || (y > w - 1))
+			{
+				neighbors_list.remove(p);
+			}
+		}
+		return neighbors_list;
 	}
 }
--- src/seamCarving/seamCarving.java
+++ src/seamCarving/seamCarving.java
@@ -3,6 +3,8 @@ package seamCarving;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.util.List;
+
 import javax.imageio.ImageIO;
 
 import RayTracing.RayTracer.RayTracerException;
@@ -10,6 +12,39 @@ public class seamCarving {
 
 	public enum EnergyTypes {
 	    REGULAR_WITHOUT_ENTROPY, REGULAR_WITH_ENTROPY, FORWARD_ENERGY
+	}
+	
+	public static int[][] compute_energy_mat_from_image(BufferedImage img)
+	{
+		int [][] energy_matrix = new int [img.getWidth()][img.getHeight()];
+		int sum = 0;
+		int neighbors_num = 0;
+		
+		for(int a = 0; a < img.getWidth(); a++)
+		{
+			for(int b = 0; b < img.getHeight(); b++)
+			{
+				Pixel p = new Pixel(a, b);
+				List<Pixel> neighbors_list = p.get_neighbors(img.getWidth(), img.getHeight());
+				neighbors_num = neighbors_list.size();
+				int [] rgb_self = img.getRGB(a, b, img.getWidth(), img.getHeight(), null, 0, img.getWidth());
+				while(neighbors_list.size() > 0)
+				{
+					Pixel neighbor = neighbors_list.remove(0);
+					int [] rgb_neighbor = img.getRGB(neighbor.x, neighbor.y, img.getWidth(), img.getHeight(), null, 0, img.getWidth());
+					sum = sum + Math.abs(rgb_neighbor[0]-rgb_self[0]) + Math.abs(rgb_neighbor[1]-rgb_self[1]) + Math.abs(rgb_neighbor[2]-rgb_self[2]);
+				}
+				energy_matrix[a][b] = sum / neighbors_num; // normalize it to number of neighbors.
+			}
+		}
+		//TODO: compute the normalized energy for each pixel.
+		return energy_matrix;
+	}
+	
+	public static Seam pick_next_seam(int [][] energy_mat)
+	{
+		//TODO: implement dynamic programming seam picking
+		return null;
 	}
 	
 	public static void main(String[] args) {
@@ -26,8 +61,14 @@ public class seamCarving {
 	      int delta_cols = input_image.getHeight() - cols_num_after;
 	      int delta_rows = input_image.getWidth() - rows_num_after;
 	      
-	      // work with the image here :
-	      //TODO: first understand how many rows and cols should be added or removed
+	      int [][] energy_mat = compute_energy_mat_from_image(input_image);
+	      
+	      //TODO: Decide to remove row or col. If a col generate a transposed energy matrix.
+	      
+	      Seam lowest_energy_seam= pick_next_seam(energy_mat);
+	      
+	      //TODO: Remove / Duplicate the chosen seam.
+	      
 	    } 
 	    catch (IOException e)
 	    {
