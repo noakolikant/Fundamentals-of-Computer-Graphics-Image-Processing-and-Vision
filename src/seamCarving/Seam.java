15ef1b927c9c0dc80b429dd3b337c8790269f41c
ophirarie <ophirarie@gmail.com>
2016-06-08 22:17:08 +0300

    add seam.java

--- src/seamCarving/Seam.java
+++ src/seamCarving/Seam.java
@@ -48,6 +48,17 @@ public class Seam {
 		}
 	}
 	
+	public void add_one_to_row(Seam other) {
+		for (int i = 0; i < this.pixels_list.size(); i++) {
+			if (other.pixels_list.get(i).row_number <= this.pixels_list.get(i).row_number) {
+				// if the seam row number is lower-or-equal than 
+				// the current seam it means that its row-coordinate
+				// in the original image is higher in one
+				this.pixels_list.get(i).row_number += 1;
+			} 
+		}
+	}
+	
 	public void finalize()
 	{
 		this.pixels_list.clear();
